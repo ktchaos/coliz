66fc3b9fac0619edd4170b0c8af247a8f98ce2df
ktchaos <marialacerda@cc.ci.ufpb.br>
2020-03-10 09:36:46 -0300

    nova versao do projeto

---
+++ OpenGL/OpenGL/src/Aplication.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "VertexBufferLayout.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "OBJ_Loader.h"
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "CHAOS", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "ERROR!" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+	
+	float positions[] = {
+		-0.5f, -0.5f, 0.0f, 0.0f, // 0
+		0.5f, -0.5f, 1.0f, 0.0f,  // 1
+		0.5f, 0.5f, 1.0f, 1.0f,   // 2
+		-0.5f, 0.5f, 0.0f, 1.0f   // 3
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	//GLCall(glEnable(GL_BLEND));
+	//GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+	VertexArray va;
+	VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+	VertexBufferLayout layout;
+
+	layout.Push<float>(2);
+	layout.Push<float>(2);
+	va.AddBuffer(vb, layout);
+
+	IndexBuffer ib(indices, 6);
+
+	glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+
+	Shader shader("res/shaders/Basic.shader");
+	shader.Bind();
+	shader.SetUniform4f("u_Color", 0.5f, 0.4f, 0.2f, 1.0);
+
+	Texture texture("res/textures/ison.png");
+	texture.Bind();
+	shader.SetUniform1i("u_Texture", 0);
+	shader.SetUniformMat4f("u_MVP", proj);
+
+	Renderer renderer;
+	
+	/* TESTING AREA ! 
+
+	objl::Loader Loader;
+	bool loadout = Loader.LoadFile("res/objects/10064_colosseum_v1_Iteration0.obj");
+
+	if (loadout)
+	{
+		// Create/Open e1Out.txt
+		std::ofstream file("try1.txt");
+
+		// Go through each loaded mesh and out its contents
+		for (int i = 0; i < Loader.LoadedMeshes.size(); i++)
+		{
+			// Copy one of the loaded meshes to be our current mesh
+			objl::Mesh curMesh = Loader.LoadedMeshes[i];
+
+			// Print Mesh Name
+			file << "Mesh " << i << ": " << curMesh.MeshName << "\n";
+
+			// Print Vertices
+			file << "Vertices:\n";
+
+			// Go through each vertex and print its number,
+			//  position, normal, and texture coordinate
+			for (int j = 0; j < curMesh.Vertices.size(); j++)
+			{
+				file << "V" << j << ": " <<
+					"P(" << curMesh.Vertices[j].Position.X << ", " << curMesh.Vertices[j].Position.Y << ", " << curMesh.Vertices[j].Position.Z << ") " <<
+					"N(" << curMesh.Vertices[j].Normal.X << ", " << curMesh.Vertices[j].Normal.Y << ", " << curMesh.Vertices[j].Normal.Z << ") " <<
+					"TC(" << curMesh.Vertices[j].TextureCoordinate.X << ", " << curMesh.Vertices[j].TextureCoordinate.Y << ")\n";
+			}
+
+			// Print Indices
+			file << "Indices:\n";
+
+			// Go through every 3rd index and print the
+			//	triangle that these indices represent
+			for (int j = 0; j < curMesh.Indices.size(); j += 3)
+			{
+				file << "T" << j / 3 << ": " << curMesh.Indices[j] << ", " << curMesh.Indices[j + 1] << ", " << curMesh.Indices[j + 2] << "\n";
+			}
+
+			// Print Material
+			file << "Material: " << curMesh.MeshMaterial.name << "\n";
+			file << "Ambient Color: " << curMesh.MeshMaterial.Ka.X << ", " << curMesh.MeshMaterial.Ka.Y << ", " << curMesh.MeshMaterial.Ka.Z << "\n";
+			file << "Diffuse Color: " << curMesh.MeshMaterial.Kd.X << ", " << curMesh.MeshMaterial.Kd.Y << ", " << curMesh.MeshMaterial.Kd.Z << "\n";
+			file << "Specular Color: " << curMesh.MeshMaterial.Ks.X << ", " << curMesh.MeshMaterial.Ks.Y << ", " << curMesh.MeshMaterial.Ks.Z << "\n";
+			file << "Specular Exponent: " << curMesh.MeshMaterial.Ns << "\n";
+			file << "Optical Density: " << curMesh.MeshMaterial.Ni << "\n";
+			file << "Dissolve: " << curMesh.MeshMaterial.d << "\n";
+			file << "Illumination: " << curMesh.MeshMaterial.illum << "\n";
+			file << "Ambient Texture Map: " << curMesh.MeshMaterial.map_Ka << "\n";
+			file << "Diffuse Texture Map: " << curMesh.MeshMaterial.map_Kd << "\n";
+			file << "Specular Texture Map: " << curMesh.MeshMaterial.map_Ks << "\n";
+			file << "Alpha Texture Map: " << curMesh.MeshMaterial.map_d << "\n";
+			file << "Bump Map: " << curMesh.MeshMaterial.map_bump << "\n";
+
+			// Leave a space to separate from the next mesh
+			file << "\n";
+		}
+
+		// Close File
+		file.close();
+	}
+	// If not output an error
+	else
+	{
+		// Create/Open e1Out.txt
+		std::ofstream file("e1Out.txt");
+
+		// Output Error
+		file << "Failed to Load File. May have failed to find it or it was not an .obj file.\n";
+
+		// Close File
+		file.close();
+	}
+
+	// Loaded Mesh Objects
+	std::vector<objl::Mesh> meshes = Loader.LoadedMeshes;
+	// Loaded Vertex Objects
+	std::vector<objl::Vertex> vertices = Loader.LoadedVertices;
+	// Loaded Index Positions
+	std::vector<unsigned int> indices = Loader.LoadedIndices;
+	// Loaded Material Objects
+	std::vector<objl::Material> material = Loader.LoadedMaterials;
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(objl::Vertex), &vertices[0], GL_STATIC_DRAW);
+
+
+	END OF TESTING AREA ! */
+
+	
+	va.Unbind();
+	vb.Unbind();
+	ib.Unbind();
+	shader.Unbind();
+	
+	float r = 0.0f;
+	float increment = 0.05f;
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		renderer.Clear();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.Bind();
+		shader.SetUniform4f("u_Color", r, 0.4f, 0.2f, 1.0);
+
+		renderer.Draw(va, ib, shader);
+		
+
+		if (r > 1.0f) {
+			increment = -0.05f;
+		}
+		else if (r < 0.0f) {
+			increment = 0.05f;
+		}
+
+		r += increment;
+		
+		/* Swap front and back buffers */
+		GLCall(glfwSwapBuffers(window));
+
+		/* Poll for and process events */
+		GLCall(glfwPollEvents());
+	}
+
+	glfwTerminate();
+	return 0;
+}
